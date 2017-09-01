87e5d6b646e0a4d1f536f17623faf8520f1b5b99
walisonjose <walison.josededeus@gmail.com>
2017-09-01 13:48:45 -0300

    Testando a integração contínua

--- install.sh
+++ install.sh
@@ -8,5 +8,26 @@ chmod 600 config.txt
 mv config.txt ~/.ssh/config
 
 
-ssh l3p@medialab.ufg.br mkdir /home/l3p/travis/
+
+
+if [ $TRAVIS_BRANCH == 'continuousIntegration' ] ; then
+    # Initialize a new git repo in current dir, and push it to our server.
+    git init
+
+    git remote add deploy "$SERVER_USER@$SERVER_ADDRESS:$GIT_PATH"
+    git config user.name "Travis CI"
+    git config user.email "$DEPLOY_EMAIL"
+
+    git add .
+    git commit -m "Deploy"
+    git push --force deploy continuousIntegration -v
+else
+    echo "Not deploying, since this branch isn't master."
+fi
+
+
+
+
+
+#ssh l3p@medialab.ufg.br mkdir /home/l3p/travis/
 
