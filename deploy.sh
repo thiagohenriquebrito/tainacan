12363c0fbbdfd1c1ce246b4e112290d992861b6e
walisonjose <walison.josededeus@gmail.com>
2017-09-01 10:31:54 -0300

    Testando a integração contínua

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+if [ $TRAVIS_BRANCH == 'continuousIntegration' ] ; then
+    # Initialize a new git repo in current dir, and push it to our server.
+    git init
+        
+    git remote add deploy "$SERVER_USER@$SERVER_ADDRESS:$GIT_PATH"
+    git config user.name "Travis CI"
+    git config user.email "$DEPLOY_EMAIL"
+    
+    git add .
+    git commit -m "Deploy"
+    git push --force deploy continuousIntegration -v
+else
+    echo "Not deploying, since this branch isn't master."
+fi
